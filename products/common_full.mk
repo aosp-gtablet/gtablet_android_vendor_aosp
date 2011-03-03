80fce037f07fd4d2610e741cd7878d4d05f58874
J Parker <kornyone@gmail.com>
2011-03-03 10:34:56 -0600

    creating android_vendor_aosp

---
+++ products/common_full.mk
@@ -0,0 +1,8 @@
+# Inherit common CM stuff
+$(call inherit-product, vendor/aosp/products/common.mk)
+
+
+# Theme packages
+#include vendor/aosp/products/themes.mk
+
+
---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+add_lunch_combo aosp_harmony-eng
+PATH=$PATH:$PWD/vendor/aosp/tools ; export PATH
